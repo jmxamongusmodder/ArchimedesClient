6228171d78ea854931a48d8c2b7b8f156e7bf5da
Hellscaped <72767525+Hellscap3d@users.noreply.github.com>
2022-11-12 04:26:50 +0000

    fixed offline

--- offlinegen/gen.py
+++ offlinegen/gen.py
@@ -27,5 +27,12 @@ templatePatch("classes_js", open("../compiled/classes.js").read())
 templatePatch("eagswebrtc_js", open("../compiled/eagswebrtc.js").read())
 templatePatch("classes_server_js", open("../compiled/classes_server.js").read())
 
+templatePatch("_css_", open("../compiled/style.css").read())
+
+iconb64 = base64.b64encode(open("../compiled/favicon.png","rb").read()).decode("utf-8")
+templatePatch("_logo_", iconb64)
+
+templatePatch("_date_", os.popen("date").read())
+
 
 open("../compiled/offline.html", "w").write(template)
