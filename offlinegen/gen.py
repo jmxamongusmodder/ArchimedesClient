a1934eb94af96b21547044ee50ed6b1e6c21ed08
Hellscaped <72767525+Hellscap3d@users.noreply.github.com>
2022-11-12 04:12:03 +0000

    added offline download, fixed toryeum, and added qubit16x

---
+++ offlinegen/gen.py
@@ -0,0 +1,31 @@
+import os,base64
+
+template = open("template.html").read()
+
+def epkToBase64(epkname):
+    epk = open(f"../compiled/packs/{epkname}.epk","rb").read()
+    return base64.b64encode(epk).decode("utf-8")
+
+threebedwars = epkToBase64("3Bedwars")
+
+default = epkToBase64("default")
+
+toryeum = epkToBase64("toryeum")
+
+qubit = epkToBase64("qubit16x")
+
+def templatePatch(key, value):
+    global template
+    template = template.replace(key, value)
+
+templatePatch("default_epk", default)
+templatePatch("3Bedwars_epk", threebedwars) 
+templatePatch("toryeum_epk", toryeum)
+templatePatch("qubit16x_epk", qubit)
+
+templatePatch("classes_js", open("../compiled/classes.js").read())
+templatePatch("eagswebrtc_js", open("../compiled/eagswebrtc.js").read())
+templatePatch("classes_server_js", open("../compiled/classes_server.js").read())
+
+
+open("../compiled/offline.html", "w").write(template)
