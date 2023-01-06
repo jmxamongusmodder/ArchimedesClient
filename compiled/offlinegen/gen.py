14e445729b2381ec238bf1d4f8d6cb4502695070
Hellscaped <72767525+Hellscap3d@users.noreply.github.com>
2023-01-06 14:14:17 -0600

    Update gen.py

--- compiled/offlinegen/gen.py
+++ compiled/offlinegen/gen.py
@@ -6,11 +6,7 @@ def epkToBase64(epkname):
     epk = open(f"../packs/{epkname}.epk","rb").read()
     return base64.b64encode(epk).decode("utf-8")
 
-threebedwars = epkToBase64("3Bedwars")
-
 default = epkToBase64("default")
-
-toryeum = epkToBase64("toryeum")
 
 qubit = epkToBase64("qubit16x")
 
@@ -19,8 +15,6 @@ def templatePatch(key, value):
     template = template.replace(key, value)
 
 templatePatch("default_epk", default)
-templatePatch("3Bedwars_epk", threebedwars) 
-templatePatch("toryeum_epk", toryeum)
 templatePatch("qubit16x_epk", qubit)
 
 templatePatch("classes_js", open("../classes.js").read())
